0d8e59fe971b4dda2a34dafaa0e49d7a55b1942d
Rémi <47191601+remi-garcia@users.noreply.github.com>
2020-09-23 07:40:03 +0200

    Add type testing in bisect.jl

--- test/interval_tests/bisect.jl
+++ test/interval_tests/bisect.jl
@@ -3,30 +3,33 @@ using Test
 
 
 @testset "`bisect` function" begin
-    X = 0..1
-    @test bisect(X, 0.5) == (0..0.5, 0.5..1)
-    @test bisect(X, 0.25) == (0..0.25, 0.25..1)
+    TypesForTests = [Float16, Float32, Float64]
+    for T in TypesForTests
+        X = 0..1
+        @test bisect(X, T(0.5)) == (T(0)..T(0.5), T(0.5)..T(1))
+        @test bisect(X, T(0.25)) == (T(0)..T(0.25), T(0.25)..T(1))
 
-    @test bisect(X) == (interval(0.0, 0.49609375), interval(0.49609375, 1.0))
+        @test bisect(X) == (interval(T(0.0), T(0.49609375)), interval(T(0.49609375), T(1.0)))
 
-    X = -∞..∞
-    @test bisect(X, 0.5) == (-∞..0, 0..∞)
-    B = bisect(X, 0.75)
-    @test B[1].hi > 0
-    @test B[1].hi == B[2].lo
-    B = bisect(X, 0.25)
-    @test B[1].hi < 0
-    @test B[1].hi == B[2].lo
+        X = T(-∞)..T(∞)
+        @test bisect(X, T(0.5)) == (T(-∞)..T(0), T(0)..T(∞))
+        B = bisect(X, T(0.75))
+        @test B[1].hi > 0
+        @test B[1].hi == B[2].lo
+        B = bisect(X, T(0.25))
+        @test B[1].hi < 0
+        @test B[1].hi == B[2].lo
 
-    X = (0..1) × (0..2)
-    @test bisect(X, 0.5) == ( (0..1) × (0..1), (0..1) × (1..2) )
-    @test bisect(X, 0.25) == ( (0..1) × (0..0.5), (0..1) × (0.5..2) )
-    @test bisect(X, 1, 0.5) == ( (0..0.5) × (0..2), (0.5..1) × (0..2) )
-    @test bisect(X, 1, 0.25) == ( (0..0.25) × (0..2), (0.25..1) × (0..2) )
+        X = (T(0)..T(1)) × (T(0)..T(2))
+        @test bisect(X, T(0.5)) == ( (T(0)..T(1)) × (T(0)..T(1)), (T(0)..T(1)) × (T(1)..T(2)) )
+        @test bisect(X, T(0.25)) == ( (T(0)..T(1)) × (T(0)..T(0.5)), (T(0)..T(1)) × (T(0.5)..T(2)) )
+        @test bisect(X, T(1), T(0.5)) == ( (T(0)..T(0.5)) × (T(0)..T(2)), (T(0.5)..T(1)) × (T(0)..T(2)) )
+        @test bisect(X, T(1), T(0.25)) == ( (T(0)..T(0.25)) × (T(0)..T(2)), (T(0.25)..T(1)) × (T(0)..T(2)) )
 
-    @test bisect(X) ==  (IntervalBox(0..1, interval(0.0, 0.9921875)),
-                         IntervalBox(0..1, Interval(0.9921875, 2.0)))
+        @test bisect(X) ==  (IntervalBox(T(0)..T(1), interval(T(0.0), T(0.9921875))),
+                             IntervalBox(T(0)..T(1), Interval(T(0.9921875), T(2.0))))
 
-    X = (-∞..∞) × (-∞..∞)
-    @test bisect(X, 0.5) == ( (-∞..0) × (-∞..∞), (0..∞) × (-∞..∞))
+        X = (T(-∞)..T(∞)) × (T(-∞)..T(∞))
+        @test bisect(X, T(0.5)) == ( (T(-∞)..T(0)) × (T(-∞)..T(∞)), (T(0)..T(∞)) × (T(-∞)..T(∞)))
+    end
 end
